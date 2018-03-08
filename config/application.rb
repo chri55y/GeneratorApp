1e817d90b195c31c08a2ea02ed584e3f737060fb
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-03-08 13:54:30 -0600

    how to customize which files and filetypes get generated

---
+++ config/application.rb
@@ -0,0 +1,36 @@
+require_relative 'boot'
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module GeneratorApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # from http://guides.rubyonrails.org/generators.html
+	config.generators do |g|
+	  g.orm             :active_record # standard, rarely change
+	  g.template_engine :erb	# could be slim, haml
+	  g.test_framework  :test_unit, fixture: false	# test_unit by default, rspec would be another option
+	  g.stylesheets     false	
+	  g.javascripts     false
+	end
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+  end
+end
